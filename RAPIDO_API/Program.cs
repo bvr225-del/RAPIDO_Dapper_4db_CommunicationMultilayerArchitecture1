9406dc853675ac659351614d34628ba427de98f0
venkat <dell@DESKTOP-S8CP3CH>
2026-04-29 12:31:06 +0530

    userstory-123479-CreatedCrudOperationsAPI'SForEmployeeUsingDapper

--- RAPIDO_API/Program.cs
+++ RAPIDO_API/Program.cs
@@ -16,6 +16,12 @@ builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+//===================================================================================
+//register the dependency injection for the repository and service layers of the application in the program.cs file of the web api project using the AddScoped method   builder object. The AddScoped method is used to register a service with a scoped lifetime, which means that a new instance of the service will be created for each HTTP request and shared within that request.
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();//register the repository interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();//register the service interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Rapido_Repositorylayer/EmployeeRepository.cs
@@ -0,0 +1,91 @@
+﻿using Dapper;
+using Rapido_BusinessEntities.Interfaces;
+using Rapido_BusinessEntities.Models;
+using Rapido_BusinessEntities.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rapido_Repositorylayer
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        public EmployeeRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+        public async Task<int> AddEmployees(Employee empdetail)
+        {
+            using (IDbConnection con = _connectionFactory.HotelmanagementsqlConnectionString())
+            {
+                //In Dapper we will use the DynamicParameters class to pass data to the stored procedure input parameters.
+                //The DynamicParameters class allows us to define parameters and their values in a flexible way, making it easier to work with stored procedures that require multiple parameters or output parameters.
+                //Create object for DynamicParameters class  for Passing  data to Storedprocedure input paramaters..
+                //The first argument is the name of the parameter as defined in the stored procedure, and the second argument is the value you want to pass to that parameter.
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add(StoredprocedureParameters.EmployeeName, empdetail.empname);
+                parameters.Add(StoredprocedureParameters.EmployeeSalary, empdetail.empsalary);
+                parameters.Add(StoredprocedureParameters.Insertedvariable, DbType.Int32, direction: ParameterDirection.Output);
+                await con.ExecuteScalarAsync<int>(StoredprocedureNames.AddEmployee, parameters, commandType: CommandType.StoredProcedure);
+                int inserterdid = parameters.Get<int>(StoredprocedureParameters.Insertedvariable);
+                return inserterdid;
+
+            }
+
+        }
+
+        public async Task<bool> DeleteEmployeeById(int empid)
+        {
+            using (IDbConnection con = _connectionFactory.HotelmanagementsqlConnectionString())
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add(StoredprocedureParameters.EmployeeID, empid);
+                await con.ExecuteScalarAsync(StoredprocedureNames.DeleteEmployee, p, commandType: CommandType.StoredProcedure);
+                return true;
+            }
+
+        }
+
+        public async Task<Employee> GetEmployeeById(int empid)
+        {
+            using (IDbConnection con = _connectionFactory.HotelmanagementsqlConnectionString())
+            {
+                var p = new DynamicParameters();
+                p.Add(StoredprocedureParameters.EmployeeID, empid);
+                var result = await con.QueryAsync<Employee>(StoredprocedureNames.GetEmployeeByEmpid, p, commandType: CommandType.StoredProcedure);
+                Employee emp = result.FirstOrDefault();//FirstOrDefault() it will return the first element of the sequence or a default value if the sequence contains no elements. In this case, it will return the first Employee object from the result set or null if there are no matching records.
+                return emp;
+            }
+
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            using (IDbConnection conn = _connectionFactory.HotelmanagementsqlConnectionString())
+            {
+                var queryresult = await conn.QueryAsync<Employee>(StoredprocedureNames.GetEmployee, CommandType.StoredProcedure);
+                List<Employee> res = queryresult.ToList();
+                return res;
+            }
+
+        }
+
+        public async Task<bool> UpdateEmployee(Employee empdetail)
+        {
+            using (IDbConnection con = _connectionFactory.HotelmanagementsqlConnectionString())
+            {
+                var p = new DynamicParameters();
+                p.Add(StoredprocedureParameters.EmployeeID, empdetail.empid);
+                p.Add(StoredprocedureParameters.EmployeeName, empdetail.empname);
+                p.Add(StoredprocedureParameters.EmployeeSalary, empdetail.empsalary);
+                await con.ExecuteReaderAsync(StoredprocedureNames.UpdateEmployee, p, commandType: CommandType.StoredProcedure);
+                return true;
+            }
+
+        }
+    }
+}
