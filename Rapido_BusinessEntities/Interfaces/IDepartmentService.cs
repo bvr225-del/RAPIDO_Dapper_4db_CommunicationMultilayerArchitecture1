78a4f1c9ea6e4268ca60c0c5df7e1d5443427f46
venkat <dell@DESKTOP-S8CP3CH>
2026-04-29 12:59:24 +0530

    userstory-123480-CreatedCrudOperationsAPI'sForDepartmentUsingDapper

---
+++ Rapido_BusinessEntities/Interfaces/IDepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using Rapido_BusinessEntities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rapido_BusinessEntities.Interfaces
+{
+    public interface IDepartmentService
+    {
+        Task<List<DepartmentDto>> GetDepartments();
+        Task<DepartmentDto> GetDepartmentById(int deptid);
+        Task<int> AddDepartment(DepartmentDto deptdetail);
+        Task<string> DeleteDepartment(int departmentid);
+        Task<string> UpdateDepartment(DepartmentDto deptdetail);
+
+    }
+}
