e516ff00c78e5121c732d0fb13916e7abc0940c8
venkat <dell@DESKTOP-S8CP3CH>
2026-04-29 12:04:48 +0530

    userstory-123478-createdPostAPI'sForSignIn&SignUp

--- RAPIDO_API/Program.cs
+++ RAPIDO_API/Program.cs
@@ -1,3 +1,8 @@
+using Rapido_BusinessEntities.Interfaces;
+using Rapido_DbConnectivity;
+using Rapido_Repositorylayer;
+using Rapido_ServiceLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
