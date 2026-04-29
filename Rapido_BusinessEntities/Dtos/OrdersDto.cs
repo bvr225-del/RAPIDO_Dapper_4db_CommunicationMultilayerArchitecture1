d838c85e93038b297b3a51719fdb754e1f45b877
venkat <dell@DESKTOP-S8CP3CH>
2026-04-29 13:24:12 +0530

    userstory-123481-CreateCrudOperationAPI'sForOrdersUsingDapper

---
+++ Rapido_BusinessEntities/Dtos/OrdersDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rapido_BusinessEntities.Dtos
+{
+    public class OrdersDto
+    {
+        public int orderid { get; set; }
+
+        public string ordername { get; set; }
+
+        public string orderlocation { get; set; }
+        ////it is used for condition checking purpose in service layer and it is not a part of database table so we are not adding this property in orders model class which is used for database communication purpose.
+        public string Flag { get; set; }
+
+    }
+}
